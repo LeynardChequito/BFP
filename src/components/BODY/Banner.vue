2881c9a58646879a378668af3868c7a371fc9f6e
LeynardChequito <144776361+LeynardChequito@users.noreply.github.com>
2023-12-12 23:01:32 +0800

    Contact Info

--- src/components/BODY/Banner.vue
+++ src/components/BODY/Banner.vue
@@ -1,12 +1,19 @@
 <template>
   <v-container>
-
-
-    <v-carousel hide-delimiters ref="carousel" height="400"> <!-- Set a specific height for the carousel -->
+    <v-carousel hide-delimiters ref="carousel" height="400">
       <v-carousel-item v-for="image in filteredImages" :key="image.id">
         <v-img :src="require(`@/assets/${image.src}`)" :alt="image.alt" class="carousel-image"></v-img>
       </v-carousel-item>
     </v-carousel>
+
+    <!-- Emergency Contacts Shortcut Button with Label -->
+    <div class="emergency-call-container">
+      
+      <v-btn fab dark fixed bottom left color="red" @click="callEmergencyContacts"><v-icon>mdi-phone</v-icon>
+      <div class="emergency-call-label">Emergency Call</div>
+        
+      </v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -30,17 +37,14 @@ export default {
   },
   computed: {
     filteredImages() {
-      // Filter images based on file extension (e.g., jpeg or jpg)
       return this.images.filter((image) => /\.(jpeg|jpg|png)$/i.test(image.src));
     },
   },
   methods: {
     showNextImage() {
-      // Show the next image in the carousel
       this.$refs.carousel.next();
     },
     showPrevImage() {
-      // Show the previous image in the carousel
       this.$refs.carousel.prev();
     },
     getCurrentTime() {
@@ -51,6 +55,14 @@ export default {
         this.philippineTime = this.getCurrentTime();
       }, 60000);
     },
+    callEmergencyContacts() {
+      // Implement the logic to initiate a call to emergency contacts
+      // You can use browser-specific APIs for this purpose.
+      // For example, in a mobile app, you might use Cordova or Capacitor plugins.
+
+      // For demonstration purposes, you can show an alert.
+      alert('Calling Emergency Contacts!');
+    },
   },
   mounted() {
     this.updatePhilippineTime();
@@ -60,12 +72,28 @@ export default {
 
 <style scoped>
 .carousel-image {
-  width: 100%; 
-  height: 100%; 
+  width: 100%;
+  height: 100%;
 }
 
 .logo {
   width: 150px;
   height: auto;
+}
+
+.emergency-call-container {
+  position: fixed;
+  bottom: 50%;
+  left: 16px;
+  transform: translateY(50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.emergency-call-label {
+  margin-bottom: 8px;
+  font-size: 12px;
+  color: White;
 }
 </style>
--- src/components/BODY/RealTimeLocation.vue
+++ src/components/BODY/RealTimeLocation.vue
@@ -3,6 +3,28 @@
     <h1>Realtime Location</h1>
     <p>Latitude: {{ latitude }}</p>
     <p>Longitude: {{ longitude }}</p>
+
+    <!-- Table for Rescuers' Contact Numbers -->
+    <div class="mt-4">
+      <h2>Rescuers' Contact Numbers</h2>
+      <table class="table table-bordered table-striped">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Cellphone Number</th>
+            <th scope="col">Telephone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- Add dynamic rows here based on your rescuers data -->
+          <tr v-for="(rescuer, index) in rescuers" :key="index">
+            <td>{{ rescuer.name }}</td>
+            <td>{{ rescuer.telephoneNumber }}</td>
+            <td>{{ rescuer.cellphoneNumber }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -12,36 +34,57 @@ export default {
     return {
       latitude: null,
       longitude: null,
+      rescuers: [
+        { name: 'City Disaster Risk Reduction Management Department (Rescue/Fire)', telephoneNumber: ' (043) 288-611 (043) 288-7521', cellphoneNumber: 'Globe: 0915-744-9698, Smart: 0999-735-6447' },
+        { name: 'Calapan City Fire Station (BFP)', telephoneNumber: '(043)288-7777', cellphoneNumber: 'Globe: 0915-603-1561'},
+        // Add more rescuers as needed
+      ],
     };
   },
-  // mounted() {
-  //   this.getLocation();
-  // },
-  // methods: {
-  //   getLocation() {
-  //     if (navigator.geolocation) {
-  //       navigator.geolocation.watchPosition(
-  //         (position) => {
-  //           this.latitude = position.coords.latitude;
-  //           this.longitude = position.coords.longitude;
-  //           this.saveLocationToDatabase();
-  //         },
-  //         (error) => {
-  //           console.error(error.message);
-  //         },
-  //         { enableHighAccuracy: true, maximumAge: 0 }
-  //       );
-  //     } else {
-  //       console.error("Geolocation is not supported by this browser.");
-  //     }
-  //   },
-  //   saveLocationToDatabase() {
-  //     // Use Axios to send data to your PHP backend
-  //     this.$axios.post("/src/components/save-location.php", {  // Assuming save-location.php is in the root of your backend
-  //       latitude: this.latitude,
-  //       longitude: this.longitude,
-  //     });
-  //   },
-  // },
+  // The rest of your component remains unchanged
 };
 </script>
+
+<style scoped>
+/* Custom styles for the component */
+.table {
+  width: 100%;
+  margin-bottom: 1rem;
+  color: #212529;
+}
+
+.table th, .table td {
+  padding: 0.75rem;
+  vertical-align: top;
+  border-top: 1px solid #dee2e6;
+}
+
+.table thead th {
+  vertical-align: bottom;
+  border-bottom: 2px solid #dee2e6;
+}
+
+.table tbody + tbody {
+  border-top: 2px solid #dee2e6;
+}
+
+.table-sm th, .table-sm td {
+  padding: 0.3rem;
+}
+
+.table-bordered {
+  border: 1px solid #dee2e6;
+}
+
+.table-bordered th, .table-bordered td {
+  border: 1px solid #dee2e6;
+}
+
+.table-bordered thead th, .table-bordered thead td {
+  border-bottom-width: 2px;
+}
+
+.table-striped tbody tr:nth-of-type(odd) {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+</style>
